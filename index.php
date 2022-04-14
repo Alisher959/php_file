0d8acd3cbf27f706ba5ab196c7767dfb93310b8a
Alisher959 <85029201+Alisher959@users.noreply.github.com>
2022-04-14 16:16:01 +0500

    'initial_commit'

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+// $kurslar = 1;
+// switch ($kurslar) {
+//     case 1:
+//         echo ('Dushanba bizda kurs kuni');
+//         break;
+//     case 2:
+//         echo ('Seshanba bizda kurs kuni');
+//         break;
+//     case 3:
+//         echo ('Chorshanba bizda kurs kuni');
+//         break;
+//     default:
+//         echo 'Kurs kunlari mavjud emas yoki kiritilmagan.';
+//         break;
+// }
+// ===============================================================
+// $i = 0;
+// while ($i <= 10) {
+//     $i++;
+//     if($i==5){
+//         break;
+//     }
+// }
+// echo("Sikl shu yerda tugadi $i");
+// ================================================================
+    
+// $array = array(1,2,3,4,5,6,7,8,9,10);
+// foreach ($array as $massiv) {
+//     if ($massiv==6)
+//         continue;
+//     echo("$massiv <br>");
+// }
